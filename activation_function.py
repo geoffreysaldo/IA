a59a2336290f4dd01d2a705a496b3b8c9d437f99
marvelousJee <geoffreysaldo@gmail.com>
2018-09-27 11:39:55 +0200

    ajout du fichier de fonction d activation

---
+++ activation_function.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 27 11:23:43 2018
+
+@author: geoffrey
+"""
+
+
+
+
+def Heavyside(entry):
+    if entry > 0 :
+        return 1
+    else :
+        return 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
